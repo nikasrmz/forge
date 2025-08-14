dff458f417b67c01cd6df363ada846115013e83a
nikasrmz <nikasharmaza@gmail.com>
2025-08-14 12:16:39 +0400

    add request class and body class placeholder classes

---
+++ framework/body.py
@@ -0,0 +1,8 @@
+class Body:
+    pass
+
+class BufferedBody(Body):
+    pass
+
+class StreamingBody(Body):
+    pass
---
+++ framework/request.py
@@ -0,0 +1,16 @@
+from typing import Dict, List, Any
+from dataclasses import dataclass
+
+from framework.body import Body
+
+@dataclass(frozen=True)
+class Request:
+    method: str
+    path: str
+    http_version: str
+    query_params: Dict[str, List[str]]
+    headers: Dict[str, str]
+    body: Body
+
+    def get_header(self, header: str, default: Any = None):
+        return self.headers.get(header, default)
