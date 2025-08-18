ab4a304ed3dfb38e6bc876c4a8482ca98f481dd7
nikasrmz <nikasharmaza@gmail.com>
2025-08-18 11:03:44 +0400

    remove sketching remains

--- framework/application.py
+++ framework/application.py
@@ -3,7 +3,6 @@ from socket import socket as Socket
 from framework.router import Router
 from server.listener import Listener
 from server.connection import ConnectionHandler
-from framework.httphandler import handle_http_request
 
 class Application:
     def __init__(self, body_threshold: int = 10 * 1024 * 1024):
@@ -15,4 +14,4 @@ class Application:
             listener.run(self.create_handler)
 
     def create_handler(self, connection_socket: Socket):
-        return ConnectionHandler(connection_socket, handle_http_request)
+        return ConnectionHandler(connection_socket)
